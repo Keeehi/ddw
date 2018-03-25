31c8308f6b4f4a93719fe44387a8ec88478b57a5
honzacejhon <honza.cejhon@inspirum.cz>
2018-03-25 20:51:20 +0200

    HW2 computed relevance scores

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -1,26 +1,48 @@
 # import
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics.pairwise import euclidean_distances
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
 import numpy as np
 
 # prepare corpus
+corpus_d = []
+corpus_q = []
 corpus = []
 for d in range(1400):
     f = open("./d/" + str(d + 1) + ".txt")
-    corpus.append(f.read())
+    text = f.read()
+    corpus_d.append(text)
+    corpus.append(text)
 
 # add query to corpus
-for q in [1]:
-    f = open("./q/" + str(q) + ".txt")
-    corpus.append(f.read())
+for q in range(225):
+    f = open("./q/" + str(q + 1) + ".txt")
+    text = f.read()
+    corpus_q.append(text)
+    corpus.append(text)
 
-# init vectorizer
+# init vectorizers
+binary_vectorizer = CountVectorizer(binary=True)
+tf_vectorizer = CountVectorizer()
 tfidf_vectorizer = TfidfVectorizer()
 
-# prepare matrix
-tfidf_matrix = tfidf_vectorizer.fit_transform(corpus)
+# prepare matrices
+matrices = {
+    "binary_matrix": binary_vectorizer.fit_transform(corpus),
+    "tf_matrix": tf_vectorizer.fit_transform(corpus),
+    "tfidf_matrix": tfidf_vectorizer.fit_transform(corpus)
+}
 
-# compute similarity between query and all docs (tf-idf) and get top 10 relevant
-sim = np.array(cosine_similarity(tfidf_matrix[len(corpus) - 1], tfidf_matrix[0:(len(corpus) - 1)])[0])
-topRelevant = sim.argsort()[-10:][::-1] + 1
-print(topRelevant)
+# compute cosine similarity and euclidian distance between all queries and all docs for all weightings
+for index in matrices:
+    print(index)
+    X = matrices[index][len(corpus_d):len(corpus) - 1]
+    Y = matrices[index][0:(len(corpus_d) - 1)]
+    cosine_similarity_array = cosine_similarity(X, Y)
+    euclidean_distances_array = euclidean_distances(X, Y)
+    np.savetxt('./o/' + index + '-cosine_similarity.csv', cosine_similarity_array, delimiter=',')
+    np.savetxt('./o/' + index + '-euclidean_distances.csv', euclidean_distances_array, delimiter=',')
+
+    # topRelevant = cosine_similarity_array.argsort()[-10:][::-1] + 1
+    # print(topRelevant)
